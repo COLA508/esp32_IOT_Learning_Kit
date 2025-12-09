5b4e00646175a70c6dc3aff0d7148dda76652ec8
COLA508 <1849377878@QQ.COM>
2025-12-09 14:20:19 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,6 @@ copybutton_icon = "📋"       # 复制按钮图标，可改为其他符号
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 
-
 html_logo = "_static/logo.png"
 
 # 静态资源目录（必须保留 _static）
